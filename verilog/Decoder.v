72b1b13dc02edb92508575668a7482d99c6f56cd
gurupunskill <gurupungavn@gmail.com>
2017-11-07 14:34:45 +0530

    Fixes decoder

--- verilog/Decoder.v
+++ verilog/Decoder.v
@@ -1,63 +1,29 @@
-
-//Module to implement a 4 bit counter=> Each time it reaches 8 it return one
-module counter4Bit(clock, isItEight);
-	
-	input 		clock;
-	output reg	isItEight;					//Every eight cycle we need to load into the registers in the PISO
-
-	integer i = 0;
-	
-	always @ (posedge clock)
-	begin
-		i = i + 1;
-
-		if(i == 8)
-		begin
-			isItEight = 1;
-			
-		end
-
-		else if(i == 9)
-		begin
-			isItEight = 0;
-			i = 0;
-		end
-
-	end
-endmodule
-
 //Module to implement SIPO
-module SIPO(clock, serialIn, message);
+module SIPO(clock, serialIn, parallelOut);
 
 	input			clock;
 	input			serialIn;
-	output reg [7:0]	message;
+	output reg [7:0]	parallelOut;
 
-	wire		_load, serialIn;
-	reg [7:0] 	tempMessage = 0;
-	reg		load;
+	wire 		load;
+	reg [7:0] 	tempparallelOut = 0;
 	integer 	i;	
 
-	counter4Bit	counter(.clock(clock), .isItEight(_load));
+	counter4Bit	counter(.clock(clock), .isItEight(load));
 
+	always @(posedge load)
+		parallelOut = tempparallelOut;
+	
 	always @ (posedge clock)
 	begin
 
-		load = ~(_load);					//If load is 1=> shift,  else load
-		
 		if(load == 0)
-			message = tempMessage;
-
-		if(load == 1)
 		begin
-
-			for(i = 1 ; i < 7 ; i = i + 1)
+			for(i = 1 ; i < 8 ; i = i + 1)
 				begin
-
-					tempMessage[i - 1] = tempMessage[i];
-
+					tempparallelOut[i - 1] = tempparallelOut[i];
 				end
-			tempMessage[7] = serialIn;
+			tempparallelOut[7] = serialIn;
 		end	
 	end
 endmodule
